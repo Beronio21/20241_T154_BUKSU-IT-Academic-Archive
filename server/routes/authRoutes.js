de223cd41db3153d18e9510297108c7a1846b941
JulianSedo31 <2001104249@student.buksu.edu.ph>
2024-11-03 22:35:28 +0800

    version2

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// server/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController'); // Make sure this controller exists
-
-// POST: Login Route
-router.post('/login', authController.login);
-
-module.exports = router;
