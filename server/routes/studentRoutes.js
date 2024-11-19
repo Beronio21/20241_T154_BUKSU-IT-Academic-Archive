ea3a680a17bb27f497723f079ca08a621ca381b7
Rebatoo <2201104172@student.buksu.edu.ph>
2024-11-19 09:35:29 +0800

    fix the error

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -12,7 +12,7 @@ router.post('/login', studentController.loginStudent);
 router.get('/', studentController.getStudents);
 
 // GET: Retrieve a specific student by ID (requires authentication)
-router.get('/:id', authMiddleware, studentController.getStudentById);
+router.get('/:id',studentController.getStudentById);
 
 // PATCH: Update an existing student by ID (requires authentication)
 router.patch('/update/:id', studentController.updateStudent);
@@ -21,10 +21,10 @@ router.patch('/update/:id', studentController.updateStudent);
 router.delete('/delete/:id', studentController.deleteStudent);
 
 // PATCH: Partially update student fields by student_id (requires authentication)
-router.patch('/:student_id', authMiddleware, studentController.patchStudent);
+router.patch('/:student_id',  studentController.patchStudent);
 
 // GET: Search students based on criteria (name, department, student_id) (requires authentication)
-router.get('/search', authMiddleware, studentController.searchStudents);
+router.get('/search', studentController.searchStudents);
 
 // Student Logout route
 router.post('/logout', studentController.logoutStudent);
