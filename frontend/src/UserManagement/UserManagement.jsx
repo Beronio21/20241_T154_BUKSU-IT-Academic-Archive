43edd7fe34465af428a3e6d3a81fdb2ed9f5744b
Beronio21 <2201107938@student.buksu.edu.ph>
2024-12-02 09:08:07 +0800

    issue admin layout

--- frontend/src/UserManagement/UserManagement.jsx
+++ frontend/src/UserManagement/UserManagement.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../Styles/TeacherRegister.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const UserManagement = () => {
